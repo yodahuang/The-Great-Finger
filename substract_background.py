a445299d4644610355fc9d866c14cd3866b578ce
Yanda Huang <yodaa@umich.edu>
2016-11-16 14:48:42 -0500

    Output frames into folder

--- substract_background.py
+++ substract_background.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
  
-cap = cv2.VideoCapture('test.mp4')
+cap = cv2.VideoCapture('Yanda_video/huang.mp4')
  
 fgbg = cv2.createBackgroundSubtractorMOG2(varThreshold = 64, detectShadows = False)
 
@@ -16,7 +16,8 @@ while(1):
 
     if not count%10:
         fgmask = fgbg.apply(frame)
-        cv2.imshow('frame',fgmask)       
+        cv2.imwrite('people_mask/frame{}.png'.format(count), fgmask)
+        #cv2.imshow('frame',fgmask)       
         k = cv2.waitKey(30) & 0xff
         if k == 27:
             break
