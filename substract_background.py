de7b1b522f0ed39907a1c152befd0eb43324dac5
Yanda Huang <yodaa@umich.edu>
2016-12-01 15:20:09 -0500

    Add mean smoother

--- substract_background.py
+++ substract_background.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
  
-cap = cv2.VideoCapture('Yanda_video/huang.mp4')
+cap = cv2.VideoCapture('Dataset/video/Yanda_1/huang.mp4')
  
 fgbg = cv2.createBackgroundSubtractorMOG2(varThreshold = 64, detectShadows = False)
 
@@ -16,6 +16,7 @@ while(1):
 
     if not count%10:
         fgmask = fgbg.apply(frame)
+        fgmask = cv2.medianBlur(fgmask, 5)
         cv2.imwrite('people_mask/frame{}.png'.format(count), fgmask)
         #cv2.imshow('frame',fgmask)       
         k = cv2.waitKey(30) & 0xff
