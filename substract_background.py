2238efe501672626bab9ba1aa84a48a8d87bfd13
Yanda Huang <yodaa@umich.edu>
2016-12-01 21:17:04 -0500

    Add median and still testing

--- substract_background.py
+++ substract_background.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2
+from matplotlib import pyplot as plt
  
 cap = cv2.VideoCapture('Dataset/video/Yanda_1/huang.mp4')
  
@@ -16,9 +17,25 @@ while(1):
 
     if not count%10:
         fgmask = fgbg.apply(frame)
-        fgmask = cv2.medianBlur(fgmask, 5)
-        cv2.imwrite('people_mask/frame{}.png'.format(count), fgmask)
-        #cv2.imshow('frame',fgmask)       
+        # Basic smoothing
+        fgmask = cv2.medianBlur(fgmask, 3)
+
+        [h,w] = fgmask.shape
+        body_points = []
+
+        for i in range(h):
+            for w in range(w):
+                if (fgmask[i,w]>0):
+                    body_points.append([i,w])
+
+        body_points = np.array(body_points)
+        [x_center, y_center] = np.median(body_points, axis=0)
+        print(x_center, y_center)
+        [x_var, y_var] = np.sqrt(np.var(body_points, axis=0))
+
+        plt.imshow(fgmask)        
+        #cv2.imwrite('people_mask/frame{}.png'.format(count), fgmask)
+        cv2.imshow('frame',fgmask)       
         k = cv2.waitKey(30) & 0xff
         if k == 27:
             break
