34d6897750951cf30b57a80d7c3651645ed4e7b7
Yanda Huang <yodaa@umich.edu>
2016-12-03 21:19:53 -0500

    Fix demo bug and add comments

--- Calibrate_demo/distance_demo.m
+++ Calibrate_demo/distance_demo.m
@@ -6,9 +6,11 @@ screen_r = [753, 257];
 load('stereo_params.mat');
 I1 = imread('arm1.png');
 I2 = imread('arm2.png');
+% Undistort the images
 I1 = undistortImage(I1,stereoParams.CameraParameters1);
 I2 = undistortImage(I2,stereoParams.CameraParameters2);
 fist_point3d = triangulate(fist_l, fist_r, stereoParams);
-fist_distanceInMeters = norm(point3d)/1000;
+fist_distanceInMeters = norm(fist_point3d)/1000;
 screen_point3d = triangulate(screen_l, screen_r, stereoParams);
-screen_distanceInMeters = norm(point3d)/1000;
+screen_distanceInMeters = norm(screen_point3d)/1000;
+%Here we get distance in z axis
--- substract_background.py
+++ substract_background.py
@@ -4,15 +4,11 @@ from matplotlib import pyplot as plt
 import os
 import scipy.io as sio
  
-cap = cv2.VideoCapture('Dataset/video/sample.mp4')
+cap = cv2.VideoCapture('./sample.mp4')
  
 fgbg = cv2.createBackgroundSubtractorMOG2(varThreshold = 64, detectShadows = False)
 
 count = 0
-
-mask_directory = 'people_mask/'
-if not os.path.exists(mask_directory):
-    os.makedirs(mask_directory)
 
 borders = []
 
@@ -21,7 +17,9 @@ while(1):
     ret, frame = cap.read()
 
     if not ret:
+        print('Stop')
         break
+
     fgmask = fgbg.apply(frame)
     # Basic smoothing
     fgmask = cv2.medianBlur(fgmask, 3)
@@ -31,23 +29,13 @@ while(1):
     xbody = []
     ybody = []
 
+
+    #Use percentile to filter out outliers
     for i in range(h):
         for w in range(w):
             if (fgmask[i,w]>0):
                 xbody.append(i)
                 ybody.append(w)
-                # body_points.append([i,w])
-
-    # body_points = np.array(body_points)
-    # [x_center, y_center] = np.median(body_points, axis=0)
-    # print(x_center, y_center)
-    # [x_var, y_var] = np.sqrt(np.var(body_points, axis=0))
-
-    # multi = 1
-    # left = x_center - multi * x_var
-    # right = x_center + multi * x_var
-    # top = y_center - multi * y_var
-    # bottom = y_center + multi * y_var
 
     thresh = 1
     if xbody and ybody:
@@ -60,27 +48,9 @@ while(1):
         borders.append([left, right, top, bottom])
     else:
         borders.append([NaN, NaN, NaN, NaN])
-        # fgmask[left:right, top] = 128
-        # fgmask[left:right, bottom] = 128
-        # fgmask[left, top:bottom] = 128
-        # fgmask[right, top:bottom] = 128
 
-    # if the lines above doesn't work, use below
-    # for x in range(left,right):
-    #     fgmask[x,top] = 128
-    #     fgmask[x,bottom] = 128
-    # for y in range(top,bottom):
-    #     fgmask[left,y] = 128
-    #     fgmask[right,y] = 128
-
-    # plt.imshow(fgmask)        
-    # #cv2.imwrite(mask_directory + 'frame{}.png'.format(count), fgmask)
-    # cv2.imshow('frame',fgmask)       
-    # k = cv2.waitKey(30) & 0xff
-    # if k == 27:
-    #     break
     print(count)
     count+=1
 cap.release()
-#cv2.destroyAllWindows()
+# Save as mat
 sio.savemat('borders.mat', mdict={'borders':borders})
