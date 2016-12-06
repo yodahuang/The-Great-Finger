31e1d3f9cf49637d1550e5d66aee113e8b69cf23
Yanda Huang <yodaa@umich.edu>
2016-12-06 18:08:56 -0500

    Update get_mouse for format

--- mouse_select_main.m
+++ mouse_select_main.m
@@ -1,16 +1,19 @@
 function mouse_select_main(target, filePath, start, ending, radius, outName)
     %Example: mouse_select_main(true, 'Dataset/sample_frames/', 2, 5, 2, 'yanda.mat');
-    if (target)
-        outDir = 'Dataset/True_skin/';
-    else
-        outDir = 'Dataset/False_skin/';
-    end
+    outDir = strcat('Dataset/SkinClassifier/',int2str(radius), '/');
     totalFileList = cellstr(ls(strcat(filePath, '*.png')));
     selectedFiles = totalFileList(start+1:ending+1);
     selectedPoints = [];
     for k = 1:length(selectedFiles)
         imageName = selectedFiles{k};
         selectedPoints = cat(1, mouse_select(strcat(filePath, imageName), radius), selectedPoints);
+    end
+    [~, ~, dim] = size(selectedPoints);
+    assert(dim==5);
+    if (target)
+        selectedPoints(:,:,dim+1) = 2;
+    else
+        selectedPoints(:,:,dim+1) = 1;
     end
     save(strcat(outDir, outName), 'selectedPoints');
 end
