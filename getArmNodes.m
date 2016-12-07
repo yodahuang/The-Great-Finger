942f3d93ebd9d8aef2bcae3b0b0d67a53af5cb65
Yilei An <yileian@umich.edu>
2016-12-07 00:10:56 -0500

    arm nodes function, now features sequence of frames

--- getArmNodes.m
+++ getArmNodes.m
@@ -1,14 +1,21 @@
-load('Dataset/video/5s/leftbackground.mat')
-load('Dataset/video/5s/leftborders.mat')
+function nodes = getArmNodes(Left)
+if Left == true
+    pre = 'left';
+else
+    pre = 'right';
+end
+load(sprintf('Dataset/video/5s/%sbackground.mat',pre))
+load(sprintf('Dataset/video/5s/%sborders.mat',pre))
 load('yanda_model.mat')
 
-frame_folder = 'Dataset/video/5s/leftframes/';
+frame_folder = sprintf('Dataset/video/5s/%sframes/',pre);
 
 framelist = dir(frame_folder);
 background_norm = double(background) ./ repmat(sum(background, 3),1,1,3) * 300;
+nodes = [];
 
 % for each frame
-for i=11:11
+for i=11:20
     img = imread(strcat(frame_folder, framelist(i+2).name));
     mask = uint8(zeros(size(img)));
     mask(borders(i,1):borders(i,2), borders(i,3):borders(i,4), :) = ones(borders(i,2)-borders(i,1)+1, borders(i,4)-borders(i,3)+1,3);
@@ -25,9 +32,9 @@ for i=11:11
     img = img .* uint8(repmat(skin_prob,1,1,3));
 %     figure;
 %     imshow(img);
-    figure;
-    imshow(skin_prob>0.9);
-    hold on
+%     figure;
+%     imshow(skin_prob>0.9);
+%     hold on
     [row, column] = find(skin_prob>0.9);
     skin_indexs = cat(2, row, column);
     % May need to change the params
@@ -53,9 +60,12 @@ for i=11:11
     rind = int32(round(0.99 * len));
     lp = [sortedX(lind), sortedY(lind)];
     rp = [sortedX(rind), sortedY(rind)];
-    disp(lp);
-    disp(rp);
-    scatter(inline_points(:,2), inline_points(:,1));
-    scatter(lp(2), lp(1), 100, 5, 'filled');
-    scatter(rp(2), rp(1), 100, 5, 'filled');
+    nodes = [nodes; lp, rp];
+    
+%     scatter(inline_points(:,2), inline_points(:,1));
+%     scatter(lp(2), lp(1), 100, 5, 'filled');
+%     scatter(rp(2), rp(1), 100, 5, 'filled');
+%     hold off;
 end
+
+
