061f11f4540d9616393b014ddef5a0117b1c141e
tianmul <tianmu@umich.edu>
2016-12-07 08:48:19 +0800

    delete some useless variables

--- Calibrate_demo/intersectLinePlane.m
+++ Calibrate_demo/intersectLinePlane.m
@@ -1,9 +1,7 @@
 function PT = intersectLinePlane(LINE, PLANE)
 %Line = [x1 y1 z1; x2 y2 z2];
 %Plane = [x1 y1 z1; x2 y2 z2; x3 y3 z3];
-%PT = [x y z], point at screen
-Line = [-2 -2 -2; 2 2 2];
-Plane = [0 0 1; 0 1 0; 1 0 0];
+%PT = [x; y; z], point at screen
 tempmatrix = [Line(1,:)' - Line(2,:)', Plane(2,:)' - Plane(1,:)', Plane(3,:)' - Plane(1,:)'];
 tempmatrix = tempmatrix^-1;
 tuv = tempmatrix * [Line(1,:)' - Plane(1,:)'];
