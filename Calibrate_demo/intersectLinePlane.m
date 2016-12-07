a7766bb95852b55e2be8bd1a9935560dbb7ed7db
tianmul <tianmu@umich.edu>
2016-12-07 09:02:15 +0800

    Add a better function

--- Calibrate_demo/intersectLinePlane.m
+++ Calibrate_demo/intersectLinePlane.m
@@ -1,10 +1,15 @@
-function PT = intersectLinePlane(LINE, PLANE)
+function [PT, Onplain] = intersectLinePlane(LINE, PLANE)
 %Line = [x1 y1 z1; x2 y2 z2];
 %Plane = [x1 y1 z1; x2 y2 z2; x3 y3 z3];
-%PT = [x; y; z], point at screen
-tempmatrix = [Line(1,:)' - Line(2,:)', Plane(2,:)' - Plane(1,:)', Plane(3,:)' - Plane(1,:)'];
+%PT = [x; y; z], intersection point at screen
+%Onplain = 1 or 0, 1 means it on the plain
+tempmatrix = [LINE(1,:)' - LINE(2,:)', PLANE(2,:)' - PLANE(1,:)', PLANE(3,:)' - PLANE(1,:)'];
 tempmatrix = tempmatrix^-1;
-tuv = tempmatrix * [Line(1,:)' - Plane(1,:)'];
-PT = Line(1,:)' + (Line(2,:)' - Line(1,:)')*tuv(1);
-
+tuv = tempmatrix * [LINE(1,:)' - PLANE(1,:)'];
+PT = LINE(1,:)' + (LINE(2,:)' - LINE(1,:)')*tuv(1);
+if(tuv(2) + tuv(3) <= 1 && tuv(2) >=0 && tuv(3) >= 0)
+    Onplain = 1;
+else
+    Onplain = 0;
+end
 end
